c1d8531d3ca327a75ebdf7869c6e2abed0a77759
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-01-11 00:11:11 +0300

    apartment feature list

--- src/components/home/All_Apartments.js
+++ src/components/home/All_Apartments.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import Card from './Card';
+import './home.css'
 
 function All_Apartments() {
-  return (
-    <div>All_Apartments</div>
-  )
+  const [apartments, setApartments] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/apartments")
+    .then((res) => res.json())
+    .then((resp) => {
+        setApartments(resp)
+    });
+},[])
+
+    return (
+        <div>
+        <div className='rentals'>
+                    <h2>Apartments Listings</h2>
+                    <div className='rental-card'>
+                        {apartments.map((card) => {
+                        return(
+                        <Card key={card.id} card={card}/>)
+                    })}
+                    </div>
+        </div>
+        </div>
+    )
 }
 
 export default All_Apartments
