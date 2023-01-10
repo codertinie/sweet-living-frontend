dffd5f747887c79c1484a334e00ebf8d9fbc6729
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2023-01-10 15:16:55 +0300

    add all routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
-// import Home from "./components/Home";
-// import Landing from "./components/landing/index.js";
-// import Footer from "./components/footer";
-// import Login from "./components/login";
-// import Signup from "./components/signup";
-// import Singleproperty from "./components/singleproperty";
+import Home from "./components/home";
+import Landing from "./components/landing";
+import Footer from "./components/footer";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Singleproperty from "./components/singleproperty";
 import Navbar from "./components/navbar";
-// import Contact from "./components/contact";
-// import About from "./components/about";
-// import Nopage from "./components/nopage";
+import Contact from "./components/contact";
+import About from "./components/about";
+import Nopage from "./components/nopage";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          {/* <Route index element={<Landing />} />
-          <Route path="home" element={<Home />} >
-            <Route index element={<Footer />} />
-          </Route>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="home" element={<><Home /> <Footer/></>} />
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
           <Route path="contact" element={<Contact />} />
           <Route path="singleproperty" element={<Singleproperty />} />
-          <Route path="*" element={<Nopage />} /> */}
-        </Route>
-      </Routes>
+          <Route path="*" element={<Nopage />} />
+        </Routes>
+
+      </>
+   
   );
 }
 
