600773038e7d3a926fe72bfbabe613d42eedd841
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2023-01-10 11:25:59 +0300

    create navbar and styling

---
+++ src/components/navbar/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavLink} from 'react-router-dom';
+import "../style.css"
+
+
+
+function Navbar(){
+
+    return(
+        <nav>
+             <div>
+                <NavLink   to="/"  className="navlink" id='header'>
+                SWEET-LIVING 
+                </NavLink>
+             </div>
+        
+           <div className='container1-nav'>
+                <NavLink   to="/home" className="navlink">
+                    Home
+                </NavLink>
+                <NavLink   to="/about" className="navlink">
+                    About
+                </NavLink>
+                <NavLink   to="/contact" className="navlink">
+                    Contact us
+                </NavLink>
+               
+           </div>
+           <div>
+           <NavLink   to="/login" className="navlink">
+                <button id='navButton'>Signin</button>
+            </NavLink>
+           </div>
+           
+        </nav>
+    )
+}
+
+export default Navbar;
+
