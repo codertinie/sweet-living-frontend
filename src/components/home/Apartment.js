a87ddecf110576412886481f7dbb1aa42ccc2015
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-01-10 23:56:20 +0300

    make carousel responsive

--- src/components/home/Apartment.js
+++ src/components/home/Apartment.js
@@ -7,6 +7,24 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function Apartment() {
     const [apartments, setApartments] = useState([])
+    
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4,
+            slidesToSlide: 3 
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 3,
+            slidesToSlide: 2  
+        },
+            mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 
+        }
+    }
 
     useEffect(() => {
         fetch("http://localhost:3000/apartments")
@@ -19,7 +37,7 @@ function Apartment() {
     return (
         <div className='apartment'>
             <h3><strong>Apartment Listings</strong></h3>
-            <Carousel >
+            <Carousel responsive={responsive} >
                 {apartments.filter((card,ind) => ind < 6).map(card => <Card card={card}/>)}
             </Carousel>
         </div>
--- src/components/home/House.js
+++ src/components/home/House.js
@@ -8,6 +8,24 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 function House() {
     const [houses, setHouses] = useState([])
 
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4,
+            slidesToSlide: 3 
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 3,
+            slidesToSlide: 2  
+        },
+            mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 
+        }
+    }
+    
     useEffect(() => {
         fetch("http://localhost:3000/houses")
         .then((res) => res.json())
@@ -20,7 +38,7 @@ function House() {
     return (
         <div className='house'>
             <h3><strong>Houses Listings</strong></h3>
-            <Carousel>
+            <Carousel responsive={responsive} >
                 {houses.filter((card,ind) => ind < 6).map(card => <Card card={card}/>)}
             </Carousel>        
         </div>
