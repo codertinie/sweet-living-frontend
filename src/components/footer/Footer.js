d10f655853bd6bfee549a7c868de5ba35c07f004
janekigenyi <janekigenyi@gmail.com>
2023-01-11 12:15:43 +0300

    FH-21 completed footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './Footer.css'
-import { FaPhoneSquareAlt, FaRegEnvelope,  FaGithubSquare, } from "react-icons/fa";
+import { FaPhoneSquareAlt, FaRegEnvelope, FaWhatsapp, FaTwitter, FaGithubSquare, FaFacebook, FaWhatsappSquare, } from "react-icons/fa";
 
 
 function Footer() {
   return(
     <div className='footer'>
       <div className='section1'>
-        <h4></h4>
-      </div>
+        <h4>contact us</h4>
+        <p><FaPhoneSquareAlt/>
+                +254712222356</p>
+                <p><FaRegEnvelope/>
+                sweetliving@gmail.com</p>
+                <p><FaWhatsapp/></p>
+                <p><FaFacebook/></p>
+                <p><FaTwitter/></p>
+                <p><FaGithubSquare/>All rights reserved &trade; inc</p>
+                </div>
+                <div className='section2'>
+                <p>Copyright By SweetLiving Properties&Developers</p>
+                </div>
+      
     </div>
-  );
+  )
+    
+  
 }
 export default Footer;
