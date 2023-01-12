34a58f481801ddcc3c5e1139983d6dac51326e04
Terah <terahmumuli@gmail.com>
2023-01-12 09:46:01 +0300

    created some files to be used

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "./landingpage.css";
+import { BsChevronDoubleDown } from "react-icons/bs";
+import music from "./video1.mp4";
 
 function landing(){
+    window.addEventListener('scroll', windowScrolled)
+
+    function windowScrolled(){
+        const yScroll = window.scrollY
+        const icon = document.getElementById('scroll_down_icon')
+
+        if (yScroll >= 100){
+            icon.style.display = "none"
+        }else {
+            icon.style.display = "block"
+        }
+    }
     return(
-        <div>
-            <h1>This is will be our landing page</h1>
-        </div>
+        <>
+        
+        </>
+        // <div>
+        //     <h1>This is will be our landing page</h1>
+        // </div>
     )
 }
 
