7b9a3c0df028ebbdb274f79e2e8460f6929d119d
Terah <terahmumuli@gmail.com>
2023-01-13 11:58:13 +0300

    Done some landing page changes

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -18,7 +18,51 @@ function landing(){
     }
     return(
         <>
-        
+        <div className="overlay"></div>
+            <video playsinline="playsinline"
+                playbackRate="0.75"
+                autoplay="autoplay"
+                muted="muted"
+                loop="loop"
+                id="row-hero-video"
+            >
+        <source src={music} type="video/mp4"/>
+            </video>
+
+            <section className="landing_section">
+                <div className="landingpage_text1">
+                    <h1><strong>The Sweetest <span>Real Estate</span></strong></h1>
+                </div>
+
+                <div className="landingpage_text2">
+                    <h2><span>In Kenya, NRB</span></h2>
+                </div>
+
+                <div className="scroll_down_image_container">
+                    <BsChevronDoubleDown id="scroll_down_icon"/>
+                </div>
+
+                <div className="landingpage_buttons_width">
+                <div className="landingpage_buttons">
+                    <button className="buy_house_button">
+                        Buy House
+                    </button>
+
+                    <button className="rent_house_button">
+                        Rent House
+                    </button>
+
+                    <button className="sell_house_button">
+                        Sell House
+                    </button>
+                </div>
+                </div>
+
+                <div className="text_align">
+                    <h3><p>Buy, sell and rent real estate, search neighbourhood and get house estimates</p></h3>
+                </div>
+
+            </section>
         </>
         // <div>
         //     <h1>This is will be our landing page</h1>
